204497c1a9ff6e0b90c1b645d01048e4a70bf373
sraj04307 <66941807+sraj04307@users.noreply.github.com>
2024-07-15 19:03:54 +0530

    kk

--- src/Components/pages/Portfolio.jsx
+++ src/Components/pages/Portfolio.jsx
@@ -3,9 +3,39 @@ import Banner2 from '../Component/Banner/Banner2/Banner2'
 import Footer from '../Component/Footer/Footer'
 // import Card5 from '../Card/Card5/Card5'
 import { useEffect } from 'react'
-import img1 from './../../assets/img/portfolio/port1.png'
-import img2 from './../../assets/img/portfolio/part2.png'
-import img3 from './../../assets/img/portfolio/port3.png'
+import img1 from './../../assets/img/portfolio/1.jpg'
+import img2 from './../../assets/img/portfolio/2.jpg'
+import img3 from './../../assets/img/portfolio/3.jpg'
+import img4 from './../../assets/img/portfolio/4.jpg'
+import img5 from './../../assets/img/portfolio/5.jpg'
+import img6 from './../../assets/img/portfolio/6.jpg'
+// import img7 from './../../assets/img/portfolio/7.jpg'
+import img8 from './../../assets/img/portfolio/8.jpg'
+import img9 from './../../assets/img/portfolio/9.jpg'
+import img10 from './../../assets/img/portfolio/10.jpg'
+import img11 from './../../assets/img/portfolio/11.jpg'
+import img12 from './../../assets/img/portfolio/12.jpg'
+import img13 from './../../assets/img/portfolio/13.jpg'
+import img14 from './../../assets/img/portfolio/14.jpg'
+import img15 from './../../assets/img/portfolio/15.jpg'
+import img16 from './../../assets/img/portfolio/16.jpg'
+import img17 from './../../assets/img/portfolio/17.jpg'
+import img18 from './../../assets/img/portfolio/18.jpg'
+import img19 from './../../assets/img/portfolio/19.jpg'
+import img20 from './../../assets/img/portfolio/20.jpg'
+import img21 from './../../assets/img/portfolio/21.jpg'
+import img22 from './../../assets/img/portfolio/22.jpg'
+import img23 from './../../assets/img/portfolio/23.jpg'
+import img24 from './../../assets/img/portfolio/24.jpg'
+import img25 from './../../assets/img/portfolio/25.jpg'
+import img26 from './../../assets/img/portfolio/26.jpg'
+import img27 from './../../assets/img/portfolio/27.jpg'
+import img28 from './../../assets/img/portfolio/28.jpg'
+import img29 from './../../assets/img/portfolio/29.jpg'
+import img30 from './../../assets/img/portfolio/30.jpg'
+
+
+
 import './../Card/Card5/Card5.css'
 import '../../assets/css/portfolio.css'
 import img7 from '../../assets/img/Banner2/PORTFOLIO.jpg'
@@ -126,12 +156,37 @@ const Portfolio = () => {
 
             </div>
 
+            <div className='col-lg-6 col-12 pt-3'>
+              <div className="sr-nit-box">
+                <div className="img-box">
+                  <img className="img-fluid" src={img4} alt="" />
+                </div>
+                <div className="nit-profolio-info">
+                  <h3 className="sr-nit-inds">Technology: WordPress | MySQL | Ecommerce | Responsive </h3>
+                </div>
+
+              </div>
+
+            </div>
+            <div className='col-lg-6 col-12 pt-3'>
+              <div className="sr-nit-box">
+                <div className="img-box">
+                  <img className="img-fluid" src={img5} alt="" />
+                </div>
+                <div className="nit-profolio-info">
+                  <h3 className="sr-nit-inds">Technology: WordPress | MySQL | Ecommerce | Responsive </h3>
+                </div>
+
+              </div>
+
+            </div>
+
           </div>
           <div className={active2 ? "row active d-flex flex-wrap" : "row "}>
             <div className='col-lg-6 col-12 pt-3'>
               <div className="sr-nit-box">
                 <div className="img-box">
-                  <img className="img-fluid" src={img3} alt="" />
+                  <img className="img-fluid" src={img6} alt="" />
                 </div>
                 <div className="nit-profolio-info">
                   <h3 className="sr-nit-inds">Technology: WordPress | MySQL | Ecommerce | Responsive </h3>
@@ -140,11 +195,10 @@ const Portfolio = () => {
               </div>
 
             </div>
-
             <div className='col-lg-6 col-12 pt-3'>
               <div className="sr-nit-box">
                 <div className="img-box">
-                  <img className="img-fluid" src={img1} alt="" />
+                  <img className="img-fluid" src={img21} alt="" />
                 </div>
                 <div className="nit-profolio-info">
                   <h3 className="sr-nit-inds">Technology: WordPress | MySQL | Ecommerce | Responsive </h3>
@@ -153,11 +207,34 @@ const Portfolio = () => {
               </div>
 
             </div>
-
             <div className='col-lg-6 col-12 pt-3'>
               <div className="sr-nit-box">
                 <div className="img-box">
-                  <img className="img-fluid" src={img2} alt="" />
+                  <img className="img-fluid" src={img8} alt="" />
+                </div>
+                <div className="nit-profolio-info">
+                  <h3 className="sr-nit-inds">Technology: WordPress | MySQL | Ecommerce | Responsive </h3>
+                </div>
+
+              </div>
+
+            </div>
+            <div className='col-lg-6 col-12 pt-3'>
+              <div className="sr-nit-box">
+                <div className="img-box">
+                  <img className="img-fluid" src={img9} alt="" />
+                </div>
+                <div className="nit-profolio-info">
+                  <h3 className="sr-nit-inds">Technology: WordPress | MySQL | Ecommerce | Responsive </h3>
+                </div>
+
+              </div>
+
+            </div>
+            <div className='col-lg-6 col-12 pt-3'>
+              <div className="sr-nit-box">
+                <div className="img-box">
+                  <img className="img-fluid" src={img10} alt="" />
                 </div>
                 <div className="nit-profolio-info">
                   <h3 className="sr-nit-inds">Technology: WordPress | MySQL | Ecommerce | Responsive </h3>
@@ -172,7 +249,7 @@ const Portfolio = () => {
             <div className='col-lg-6 col-12 pt-3'>
               <div className="sr-nit-box">
                 <div className="img-box">
-                  <img className="img-fluid" src={img2} alt="" />
+                  <img className="img-fluid" src={img11} alt="" />
                 </div>
                 <div className="nit-profolio-info">
                   <h3 className="sr-nit-inds">Technology: WordPress | MySQL | Ecommerce | Responsive </h3>
@@ -185,7 +262,7 @@ const Portfolio = () => {
             <div className='col-lg-6 col-12 pt-3'>
               <div className="sr-nit-box">
                 <div className="img-box">
-                  <img className="img-fluid" src={img1} alt="" />
+                  <img className="img-fluid" src={img12} alt="" />
                 </div>
                 <div className="nit-profolio-info">
                   <h3 className="sr-nit-inds">Technology: WordPress | MySQL | Ecommerce | Responsive </h3>
@@ -198,7 +275,31 @@ const Portfolio = () => {
             <div className='col-lg-6 col-12 pt-3'>
               <div className="sr-nit-box">
                 <div className="img-box">
-                  <img className="img-fluid" src={img3} alt="" />
+                  <img className="img-fluid" src={img13} alt="" />
+                </div>
+                <div className="nit-profolio-info">
+                  <h3 className="sr-nit-inds">Technology: WordPress | MySQL | Ecommerce | Responsive </h3>
+                </div>
+
+              </div>
+
+            </div>
+            <div className='col-lg-6 col-12 pt-3'>
+              <div className="sr-nit-box">
+                <div className="img-box">
+                  <img className="img-fluid" src={img14} alt="" />
+                </div>
+                <div className="nit-profolio-info">
+                  <h3 className="sr-nit-inds">Technology: WordPress | MySQL | Ecommerce | Responsive </h3>
+                </div>
+
+              </div>
+
+            </div>
+            <div className='col-lg-6 col-12 pt-3'>
+              <div className="sr-nit-box">
+                <div className="img-box">
+                  <img className="img-fluid" src={img15} alt="" />
                 </div>
                 <div className="nit-profolio-info">
                   <h3 className="sr-nit-inds">Technology: WordPress | MySQL | Ecommerce | Responsive </h3>
@@ -213,7 +314,7 @@ const Portfolio = () => {
             <div className='col-lg-6 col-12 pt-3'>
               <div className="sr-nit-box">
                 <div className="img-box">
-                  <img className="img-fluid" src={img2} alt="" />
+                  <img className="img-fluid" src={img16} alt="" />
                 </div>
                 <div className="nit-profolio-info">
                   <h3 className="sr-nit-inds">Technology: WordPress | MySQL | Ecommerce | Responsive </h3>
@@ -226,7 +327,7 @@ const Portfolio = () => {
             <div className='col-lg-6 col-12 pt-3'>
               <div className="sr-nit-box">
                 <div className="img-box">
-                  <img className="img-fluid" src={img3} alt="" />
+                  <img className="img-fluid" src={img17} alt="" />
                 </div>
                 <div className="nit-profolio-info">
                   <h3 className="sr-nit-inds">Technology: WordPress | MySQL | Ecommerce | Responsive </h3>
@@ -239,7 +340,7 @@ const Portfolio = () => {
             <div className='col-lg-6 col-12 pt-3'>
               <div className="sr-nit-box">
                 <div className="img-box">
-                  <img className="img-fluid" src={img1} alt="" />
+                  <img className="img-fluid" src={img18} alt="" />
                 </div>
                 <div className="nit-profolio-info">
                   <h3 className="sr-nit-inds">Technology: WordPress | MySQL | Ecommerce | Responsive </h3>
@@ -248,13 +349,40 @@ const Portfolio = () => {
               </div>
 
             </div>
+
+            <div className='col-lg-6 col-12 pt-3'>
+              <div className="sr-nit-box">
+                <div className="img-box">
+                  <img className="img-fluid" src={img19} alt="" />
+                </div>
+                <div className="nit-profolio-info">
+                  <h3 className="sr-nit-inds">Technology: WordPress | MySQL | Ecommerce | Responsive </h3>
+                </div>
+
+              </div>
+
+            </div>
+
+            <div className='col-lg-6 col-12 pt-3'>
+              <div className="sr-nit-box">
+                <div className="img-box">
+                  <img className="img-fluid" src={img20} alt="" />
+                </div>
+                <div className="nit-profolio-info">
+                  <h3 className="sr-nit-inds">Technology: WordPress | MySQL | Ecommerce | Responsive </h3>
+                </div>
+
+              </div>
+
+            </div>
+
 
           </div>
           <div className={active5 ? "row active d-flex flex-wrap" : "row "}>
             <div className='col-lg-6 col-12 pt-3'>
               <div className="sr-nit-box">
                 <div className="img-box">
-                  <img className="img-fluid" src={img1} alt="" />
+                  <img className="img-fluid" src={img21} alt="" />
                 </div>
                 <div className="nit-profolio-info">
                   <h3 className="sr-nit-inds">Technology: WordPress | MySQL | Ecommerce | Responsive </h3>
@@ -267,7 +395,7 @@ const Portfolio = () => {
             <div className='col-lg-6 col-12 pt-3'>
               <div className="sr-nit-box">
                 <div className="img-box">
-                  <img className="img-fluid" src={img3} alt="" />
+                  <img className="img-fluid" src={img22} alt="" />
                 </div>
                 <div className="nit-profolio-info">
                   <h3 className="sr-nit-inds">Technology: WordPress | MySQL | Ecommerce | Responsive </h3>
@@ -280,7 +408,83 @@ const Portfolio = () => {
             <div className='col-lg-6 col-12 pt-3'>
               <div className="sr-nit-box">
                 <div className="img-box">
-                  <img className="img-fluid" src={img2} alt="" />
+                  <img className="img-fluid" src={img23} alt="" />
+                </div>
+                <div className="nit-profolio-info">
+                  <h3 className="sr-nit-inds">Technology: WordPress | MySQL | Ecommerce | Responsive </h3>
+                </div>
+
+              </div>
+
+            </div>
+
+            <div className='col-lg-6 col-12 pt-3'>
+              <div className="sr-nit-box">
+                <div className="img-box">
+                  <img className="img-fluid" src={img24} alt="" />
+                </div>
+                <div className="nit-profolio-info">
+                  <h3 className="sr-nit-inds">Technology: WordPress | MySQL | Ecommerce | Responsive </h3>
+                </div>
+
+              </div>
+
+            </div>
+            <div className='col-lg-6 col-12 pt-3'>
+              <div className="sr-nit-box">
+                <div className="img-box">
+                  <img className="img-fluid" src={img25} alt="" />
+                </div>
+                <div className="nit-profolio-info">
+                  <h3 className="sr-nit-inds">Technology: WordPress | MySQL | Ecommerce | Responsive </h3>
+                </div>
+
+              </div>
+
+            </div>
+
+            <div className='col-lg-6 col-12 pt-3'>
+              <div className="sr-nit-box">
+                <div className="img-box">
+                  <img className="img-fluid" src={img27} alt="" />
+                </div>
+                <div className="nit-profolio-info">
+                  <h3 className="sr-nit-inds">Technology: WordPress | MySQL | Ecommerce | Responsive </h3>
+                </div>
+
+              </div>
+
+            </div>
+
+            <div className='col-lg-6 col-12 pt-3'>
+              <div className="sr-nit-box">
+                <div className="img-box">
+                  <img className="img-fluid" src={img28} alt="" />
+                </div>
+                <div className="nit-profolio-info">
+                  <h3 className="sr-nit-inds">Technology: WordPress | MySQL | Ecommerce | Responsive </h3>
+                </div>
+
+              </div>
+
+            </div>
+
+            <div className='col-lg-6 col-12 pt-3'>
+              <div className="sr-nit-box">
+                <div className="img-box">
+                  <img className="img-fluid" src={img29} alt="" />
+                </div>
+                <div className="nit-profolio-info">
+                  <h3 className="sr-nit-inds">Technology: WordPress | MySQL | Ecommerce | Responsive </h3>
+                </div>
+
+              </div>
+
+            </div>
+            <div className='col-lg-6 col-12 pt-3'>
+              <div className="sr-nit-box">
+                <div className="img-box">
+                  <img className="img-fluid" src={img30} alt="" />
                 </div>
                 <div className="nit-profolio-info">
                   <h3 className="sr-nit-inds">Technology: WordPress | MySQL | Ecommerce | Responsive </h3>
